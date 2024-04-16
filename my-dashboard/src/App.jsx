056f9d0b06789be092031f818bbcb17ab2cd5a6c
Sarveshreg <sarveshregmi1@gmail.com>
2024-04-15 19:11:18 -0500

    test

---
+++ my-dashboard/src/App.jsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+export default function TemporaryDrawer() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer(true)}>Open drawer</Button>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
+
+
+
+// import { useState } from 'react';
+// import {Typography,AppBar,Toolbar,IconButton} from '@mui/material';
+// import {MenuIcon} from '@mui/icons-material/Menu';
+
+// import './App.css'
+
+// function App() {
+
+
+//   return (
+//     <>
+      
+//       <Typography variant="h1" component="h2">The Sales Dashboard</Typography>
+//       <AppBar position="static">
+//   <Toolbar variant="dense">
+//     <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+//       <MenuIcon />
+//     </IconButton>
+//     <Typography variant="h6" color="inherit" component="div">
+//       Photos
+//     </Typography>
+//   </Toolbar>
+// </AppBar>
+//     </>
+//   )
+// }
+
+// export default App
